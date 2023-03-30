e432420c005e4c271dd4a20be2596ad9034db24e
Han-Bin Song <thdgksq@naver.com>
2023-03-30 12:00:24 +0900

    Initial commit

--- A0330.py
+++ A0330.py
@@ -2,23 +2,32 @@
 # A = list(map(int, input().split()))
 # A.sort()
 # print(A[K - 1])
-def binary_search_recursion(target, start, end, data):
-    if start > end:
-        return None
-
-    mid = (start + end) // 2
-
-    if data[mid] == target:
-        return mid
-    elif data[mid] > target:
-        end = mid - 1
-    else:
-        start = mid + 1
-
-    return binary_search_recursion(target, start, end, data)
-
-
-N, M = map(int, input().split())
-A = list(map(int, input().split()))
-A.sort()
-print(binary_search_recursion(M, 0, len(A) - 1, A) + 1)
+#
+# def binary_search_recursion(target, start, end, data):
+#     if start > end:
+#         return None
+#
+#     mid = (start + end) // 2
+#
+#     if data[mid] == target:
+#         return mid
+#     elif data[mid] > target:
+#         end = mid - 1
+#     else:
+#         start = mid + 1
+#
+#     return binary_search_recursion(target, start, end, data)
+#
+#
+# N, M = map(int, input().split())
+# A = list(map(int, input().split()))
+# A.sort()
+# print(binary_search_recursion(M, 0, len(A) - 1, A) + 1)
+#
+# N, M = map(int, input().split())
+# A = list(map(int, input().split()))
+# B = list(map(int, input().split()))
+#
+# C = A + B
+# C.sort()
+# print(str(C))
