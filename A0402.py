e043bbf886538f064c4d1c470a39e9abf232684d
Han-Bin Song <thdgksq@naver.com>
2023-04-03 11:42:59 +0900

    탐욕 알고리즘 문제풀이

--- A0402.py
+++ A0402.py
@@ -42,4 +42,43 @@
 #     res += '#' + str(i) + ': ' + " ".join(reverseStr) + '\n'
 #
 # print(res[:len(res) - 1])
+#
+# N = int(input())
+#
+# letters1 = []
+# letters2 = []
+# res = ''
+#
+# for _ in range(0, N):
+#     letters1.append(input())
+#
+# for _ in range(0, N - 1):
+#     letters2.append(input())
+#
+# for letter in letters1:
+#     if letter not in letters2:
+#         res += letter
+#         break
+#
+# print(res)
 
+n = int(input())
+
+dic = {}
+
+for _ in range(0, n):
+    k, e = map(int, input().split())
+    dic[k] = e
+
+res = 0
+for k in dic.keys():
+    max_n = 0
+    a = k
+    while a in dic:
+        max_n += 1
+        a = dic[a]
+
+    if res <= max_n:
+        res = max_n
+
+print(res)
