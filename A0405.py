4347c7db3a989f7ce2ff72ba3770146c380b58f7
Han-Bin Song <thdgksq@naver.com>
2023-04-06 16:53:18 +0900

    아나그램 문제 풀이

--- A0405.py
+++ A0405.py
@@ -20,13 +20,31 @@
 #     else:
 #         res.append(max(dices) * 100)
 # print(max(res))
+#
+# N = int(input())
+# res = []
+# for _ in range(0, N):
+#     h, k = map(int, input().split())
+#     res.append((h, k))
+# for r in res:
+#     if any(h > r[0] and w > r[1] for h, w in res):
+#         res.remove(r)
+# print(len(res))
 
-N = int(input())
-res = []
-for _ in range(0, N):
-    h, k = map(int, input().split())
-    res.append((h, k))
-for r in res:
-    if any(h > r[0] and w > r[1] for h, w in res):
-        res.remove(r)
-print(len(res))
+def is_anagram(a, b):
+    if len(a) != len(b):
+        return False
+
+    a = sorted(a)
+    b = sorted(b)
+
+    return a == b
+
+
+a = input("첫 번째 단어를 입력하세요: ")
+b = input("두 번째 단어를 입력하세요: ")
+
+if is_anagram(a, b):
+    print("두 단어는 아나그램이다.")
+else:
+    print("두 단어는 아나그램이 아니다.")
